f744e8b06c7b2f3ae13dbe50f3fc7e7253d57b59
mithilesh <mithilesht@cloudthat.com>
2023-06-14 16:31:56 +0530

    Initial commit

---
+++ data/index.js
@@ -0,0 +1,4 @@
+export const data = {
+    "MAILGUN_API_KEY":"92fbba3340d55791b469754ed108fa9a-53c13666-e46ce901",
+    "MAILGUN_DOMAIN":"sandboxb3cf762d23f94797ae859038c83238c8.mailgun.org"
+}
---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const { data } = require('./data')
+const express = require('express')
+const app = express()
+var corOptions = {
+    origin:true
+}
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(cors(corOptions))
+const mailgun = require('mailgun.js')
+const mg = () => {
+    mailgun({
+        apiKey:data.MAILGUN_API_KEY,
+        domain:data.MAILGUN_DOMAIN
+    })
+}
+
+app.get('/',(req,res)=>{
+
+        res.send({message:'Welcome to Notifications Server version 1.0'})
+
+})
+
+app.post('/api/email',(req,res)=>{
+
+    const { to, subject, message } = req.body 
+    const emailInfo = {
+        from:"mithilesh.tarkar@gmail.com",
+        to:`${to}`,
+        subject:`${subject}`,
+        html:`${message}`
+    }
+    console.log(emailInfo)
+    res.send({message: 'Success'})
+
+})
+
+const PORT = process.env.PORT || 8080 
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`)
+})
