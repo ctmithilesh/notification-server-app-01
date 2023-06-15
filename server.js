b49de228b2f3b84c14c4e9a96ab3714d8f52f1f5
mithilesh <mithilesht@cloudthat.com>
2023-06-15 11:30:45 +0530

    successfully tested email functions with mailgun

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const { data } = require('./data')
@@ -11,14 +12,17 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended:true
 }))
-app.use(cors(corOptions))
-const mailgun = require('mailgun.js')
-const mg = () => {
-    mailgun({
-        apiKey: data.MAILGUN_API_KEY,
-        domain: data.MAILGUN_DOMAIN
-    })
-}
+app.use(cors(corOptions))   
+const formData = require('form-data');
+const Mailgun = require('mailgun.js')
+const mailgun = new Mailgun(formData);
+
+const mg = mailgun.client({
+    username:'api',
+    key: data.MAILGUN_API_KEY,
+    public_key: data.PUBLIC_KEY
+
+})
 
 app.get('/',(req,res)=>{
 
@@ -26,7 +30,7 @@ app.get('/',(req,res)=>{
 
 })
 
-app.post('/api/email',(req,res)=>{
+app.post('/api/email', async (req,res)=>{
 
     const { to, subject, message } = req.body 
     const emailInfo = {
@@ -36,7 +40,21 @@ app.post('/api/email',(req,res)=>{
         html:`${message}`
     }
     console.log(emailInfo)
-    res.send({message: 'Success'})
+
+    try{
+        const result = await mg.messages.create(data.MAILGUN_DOMAIN,{
+            from:"mithilesh.tarkar@gmail.com",
+            to,
+            subject,
+            message
+        })
+        console.log(result)
+        res.status(200).json({message:"Email sent successfully"})
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).json({message: 'Failed to send Email'})
+    }
 
 })
 
