6f416117488c2b407e2f50ede09a478dc5532daa
mithilesh <mithilesht@cloudthat.com>
2023-06-15 10:09:46 +0530

    bug fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const { data } = require('./data')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 var corOptions = {
     origin:true
